d23ae3b81d260479a3d14a5607c7195f52c36c87
Petr <vinokurovvin@list.ru>
2024-07-14 16:59:57 +0900

    LoggingTransport

--- yookassa_test.go
+++ yookassa_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"testing"
 
@@ -28,32 +27,6 @@ func (c *C) BasicAuth(ctx context.Context, operationName string) (ogencl.BasicAu
 		Username: os.Getenv("YOOKASSA_USER"),
 		Password: os.Getenv("YOOKASSA_PASSWORD"),
 	}, nil
-}
-
-type LoggingTransport struct {
-	Logger *slog.Logger
-	Level  slog.Level
-}
-
-func (s *LoggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
-	bytes, _ := httputil.DumpRequestOut(r, true)
-
-	resp, err := http.DefaultTransport.RoundTrip(r)
-	// err is returned after dumping the response
-
-	respBytes, _ := httputil.DumpResponse(resp, true)
-
-	if s.Logger != nil {
-		if resp.StatusCode != http.StatusOK {
-			s.Logger.Error("wrong status code", slog.Int("code", resp.StatusCode), slog.String("res", string(respBytes)), slog.String("req_out", string(bytes)))
-		}
-		if s.Level == slog.LevelDebug {
-			s.Logger.Info("response", slog.String("yookassa response", string(respBytes)))
-			s.Logger.Info("response", slog.String("yookassa DumpRequestOut", string(bytes)))
-		}
-	}
-
-	return resp, err
 }
 
 func TestXxx(t *testing.T) {
